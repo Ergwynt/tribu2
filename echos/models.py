37b7cd9ad0c07b7e660aff43463fe16e3798cb63
Ergwynt <aday18005@gmail.com>
2024-11-06 20:26:11 +0000

    Comiteo

--- echos/models.py
+++ echos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Echo(models.Model):
-    content = models.TextField(max_length=100)
+    content = models.TextField(max_length=755)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
